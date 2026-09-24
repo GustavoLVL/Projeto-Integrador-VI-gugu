e5c968e421dfe9ed43d32ebdc62bf477ddb87d49
GustavoLVL <gustavomirandaoficiallvl@gmail.com>
2026-09-24 16:08:10 +0000

    GUGU24/09

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const produtoRoutes =
-  require("./src/app.js/routes/produto.routes");
+const produtoRoutes = require("./src/app.js/routes/produto.routes");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use("/produtos", produtoRoutes);
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+// npx kill-port 3001
+// node index.js
+// https://bug-free-barnacle-7v5jpv4jjx4q3r7x6-3001.app.github.dev/produtos
